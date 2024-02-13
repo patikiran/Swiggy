e1b268a1f676a5136cb37db490d28e3cefd366cd
kiran <patikiran80@gmail.com>
2024-02-13 20:37:54 +0530

    Student id details add

---
+++ src/StudentId.java
@@ -0,0 +1,11 @@
+public class StudentId {
+    public int rollNum;
+    public int phoneNum;
+
+    public StudentId(int rollNum,int phoneNum) {
+        this.rollNum = rollNum;
+        this.phoneNum = phoneNum;
+    }
+
+    StudentId studentId = new StudentId(143,8790018627);
+}
