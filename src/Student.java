1c67b938085a7db5ce63cf588cc1e868f645db1f
kiran <patikiran80@gmail.com>
2024-02-13 20:25:57 +0530

    Student details add

---
+++ src/Student.java
@@ -0,0 +1,12 @@
+public class Student{
+
+    public String name;
+    public String fatherName;
+
+    public Student(String name, String fatherName) {
+        this.name = name;
+        this.fatherName = fatherName;
+    }
+    Student student = new Student("kirankumar","prabhakarrao");
+    System.out.println(student)
+}
